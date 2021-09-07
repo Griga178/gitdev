cec8e4d471d30a7bfec0aa7ca02c7190c1909f19
Griga178 <grisha.tis@gmail.com>
2021-09-07 17:42:08 +0300

    screen shoot

--- 6n.py
+++ 6n.py
@@ -1,25 +1,6 @@
-import requests
-import webbrowser
 
-url = 'https://my-shop.ru/shop/product/195.html?b45=1_1'
-url2 = 'https://my-shop.ru/shop/product/3747126.html'
-
-#r = requests.get(url)
-#print(r.text)
-
-#webbrowser.open(url2, new=0) #, autoraise=True
-
-
-'''
-надо выполнить код JS при помощи Py
-потом по тегу найти икскомое_
-нужен javascript knowlege or Java
-'''
-
-'''
-сохранение html кода (какой есть)
-from urllib.request import urlopen
-page = urlopen(url)
-with open("test.html", "wb") as fh:
-    fh.write(page.read())
-'''
+dict = {'grisha': [1, 2, 4], 'katya': [1, 3, 6], 'anna': [11, 12, 14], 'rob': [31, 32, 34]}
+for el in dict:
+    print(el)
+    for num in dict[el]:
+        print(num)
--- 7n.py
+++ 7n.py
@@ -1,13 +1,14 @@
 '''
-    Создаем папки с названием по компаниям
-    Подсчет количества цен (- необходимых скриншотов) по каждой компании (csv)
-    Закидываем в эти папки скриншоты (подсчет csv)
+    I
+    Есть - Создаем папки с названием по компаниям
+    Есть - Подсчет количества цен (- необходимых скриншотов) по каждой компании (csv)
+    Есть - Закидываем в эти папки скриншоты (подсчет)
 
     отдельное сохранение скриншотов в ворде
 
     запись инфы в СЭД
 '''
-import os
+
 import pandas
 
 
@@ -28,18 +29,15 @@ usecols = [column_price, column_numer, column_name, column_inn])
 list_name = df[column_name].tolist()
 list_numer  = df[column_numer].tolist()
 list_inn = df[column_inn].tolist()
-
-#print(df.shape)
-#print(df.head)
-#print(df[column_name])
-#print(df.groupby(column_name).sum())
-#print(df[column_name].str[:] + df[column_numer].str[:])
-#for el in df[column_name].unique():
+list_price = df[column_price].tolist()
 
 list_comp_name = []
-list_inn_comp = []
+
 set_otvetov = set()
 set_ekran = set()
+
+dict_otveti = {}
+dict_ekranki = {}
 
 row = 0
 count = 0
@@ -51,35 +49,119 @@ for el in list_name:
     try:
         str_len = len(list_numer[row])
         if str_len != 33:
-            if list_numer[row][:1] == "О":
+            count += 1
+            if list_numer[row][:1] == "О" and list_price[row] > 0:
                 o_count += 1
-                set_otvetov.add(el)
+                new_el = el.replace('"', '')
 
-                #list_comp_name.append(el)
-                #list_inn_comp.append(list_inn[row])
-            elif list_numer[row][:1] == "Э":
+                set_otvetov.add(new_el)
+                list_comp_name.append(new_el)
+
+                if new_el not in dict_otveti:
+                    deic_el = {new_el: [row + 2]}
+                    dict_otveti.update(deic_el)
+                else:
+                    dict_otveti[new_el].append(row + 2)
+            elif list_numer[row][:1] == "Э" and list_price[row] > 0:
+                new_el = el.replace('"', '')
+
                 e_count += 1
-                set_ekran.add(el)
-            '''
-            if el not in list_comp_name:
-                list_comp_name.append(el)
-                list_inn_comp.append(list_inn[row])
-                #print(el)
-                print(list_numer[row])
-                count += 1
-            '''
+                set_ekran.add(new_el)
+                list_comp_name.append(new_el)
+
+
+                if new_el not in dict_ekranki:
+                    deic_el = {new_el: [row + 2]}
+                    dict_ekranki.update(deic_el)
+                else:
+                    dict_ekranki[new_el].append(row + 2)
     except:
         pass
+    #print(row + 2) # первая 0, вторая - заголовки
     row += 1
 
+#                                                     тут подсчет факт с планом
+#for el in dict_ekranki:
+    #print(el, len(dict_ekranki[el]))
 
-print(o_count,e_count)
-print(count)
-print(len(set_otvetov))
-print(len(set_ekran))
 
-with open('new.csv') as f:
+import os
+import shutil
+
+main_dir = '../devfiles/ales_screenes'
+otvet_dir = '../devfiles/ales_screenes/Otveti'
+ekran_dir = '../devfiles/ales_screenes/Ekranki'
+
+def create_dirs():
+    try:
+        os.mkdir(main_dir)
+        os.mkdir(otvet_dir)
+        os.mkdir(ekran_dir)
+    except:
+        print('\nГлавные папки не создавались\n')
+
+    # СОздание папок для Ответов по компаниям
+    for el in set_otvetov:
+        dir_name = otvet_dir + '/' + el
+        try:
+            os.mkdir(dir_name)
+        except:
+            print(dir_name, '   Не создавалась')
+    # СОздание папок для Экранок по компаниям
     for el in set_ekran:
-        
+        dir_name = ekran_dir + '/' + el
+        try:
+            os.mkdir(dir_name)
+        except:
+            print(dir_name, '   Не создавалась')
 
-#print(len(list_comp_name))
+# Копируем скриншоты
+# испольюзуются: dict_ekranki, dict_otveti
+
+def copy_screen():
+    copy_count = 0
+    for el in dict_otveti:
+        direct_name = otvet_dir + '/' + el + '/'
+        #print(direct_name)
+        for num in dict_otveti[el]:
+            scr_name = str(num) + '.jpg'
+            old_screen_name = '../devfiles/scr/first912/' + scr_name
+            new_screen_name = direct_name + scr_name
+            #print(old_screen_name)
+            #print(new_screen_name)
+            try:
+                shutil.copyfile(old_screen_name, new_screen_name)
+                copy_count += 1
+            except:
+                print("Не удалось скопировать", scr_name)
+
+    print(copy_count, ' Ответов')
+
+    copy_count_ecr = 0
+    for el in dict_ekranki:
+        direct_name = ekran_dir + '/' + el + '/'
+        #print(direct_name)
+        for num in dict_ekranki[el]:
+            scr_name = str(num) + '.jpg'
+            old_screen_name = '../devfiles/scr/first912/' + scr_name
+            new_screen_name = direct_name + scr_name
+            #print(old_screen_name)
+            #print(new_screen_name)
+            try:
+                shutil.copyfile(old_screen_name, new_screen_name)
+                #print(new_screen_name)
+                copy_count_ecr += 1
+            except:
+                print("Не удалось скопировать", scr_name)
+    print(copy_count_ecr, ' Экранок')
+
+#create_dirs()
+#copy_screen()
+
+
+# Считаем количество использования источников {Ситилинк ООО; 100}
+dict_comp = {el : list_comp_name.count(el) for el in list_comp_name}
+sorted_dict = {}
+sorted_list = sorted(dict_comp, key = dict_comp.get, reverse = True)
+for comp in sorted_list:
+    sorted_dict[comp] = dict_comp[comp]
