1873c22937cd7e52b885133c785389a83e6ed2f4
Griga178 <grisha.tis@gmail.com>
2021-09-08 17:41:07 +0300

    7n update (scr_name_column)

--- 7n.py
+++ 7n.py
@@ -12,24 +12,26 @@
 import pandas
 
 
-exel_file = '../devfiles/reestr 4 ready.xlsx'
+exel_file = '../devfiles/reestr 4 (для скринов + норм вар).xlsx'
 
-sheets_name = 'RT new'
+sheets_name = 'new+'
 
 column_name = 'Наименование поставщика'
 column_inn = 'ИНН поставщика'
 column_price = 'Новая цена 4кв'
 column_numer = 'Источник ценовой информации'
+column_jpg_name = 'jpg_name'
 
 dict_main = {}
 
 df = pandas.read_excel(exel_file, sheet_name = sheets_name,
-usecols = [column_price, column_numer, column_name, column_inn])
+usecols = [column_price, column_numer, column_name, column_inn, column_jpg_name])
 
 list_name = df[column_name].tolist()
 list_numer  = df[column_numer].tolist()
-list_inn = df[column_inn].tolist()
+#list_inn = df[column_inn].tolist()
 list_price = df[column_price].tolist()
+list_jpg_name = df[column_jpg_name].tolist()
 
 list_comp_name = []
 
@@ -45,25 +47,32 @@ o_count = 0
 e_count = 0
 
 for el in list_name:
-    # el - Название компании
+    # el - Название компании -> new_el
+    # row - номер строки = имя скриншота (старое правило) + индекс "всей строки"
+    # list_jpg_name[row]
     try:
         str_len = len(list_numer[row])
         if str_len != 33:
             count += 1
+            #jpg_name = (str(int(list_jpg_name[row])) + '.jpg')
+            jpg_name = (int(list_jpg_name[row]))
+            #print(f'Номер строки: {row + 2} jpg_name: {jpg_name}')
             if list_numer[row][:1] == "О" and list_price[row] > 0:
                 o_count += 1
-                new_el = el.replace('"', '')
+                new_el = el.replace('"', '').replace('«', '').replace('»', '')
 
                 set_otvetov.add(new_el)
                 list_comp_name.append(new_el)
 
                 if new_el not in dict_otveti:
-                    deic_el = {new_el: [row + 2]}
+                    #deic_el = {new_el: [row + 2]}
+                    deic_el = {new_el: [jpg_name]}
                     dict_otveti.update(deic_el)
                 else:
-                    dict_otveti[new_el].append(row + 2)
+                    #dict_otveti[new_el].append(row + 2)
+                    dict_otveti[new_el].append(jpg_name)
             elif list_numer[row][:1] == "Э" and list_price[row] > 0:
-                new_el = el.replace('"', '')
+                new_el = el.replace('"', '').replace('«', '').replace('»', '')
 
                 e_count += 1
                 set_ekran.add(new_el)
@@ -71,26 +80,41 @@ for el in list_name:
 
 
                 if new_el not in dict_ekranki:
-                    deic_el = {new_el: [row + 2]}
+                    #deic_el = {new_el: [row + 2]}
+                    deic_el = {new_el: [jpg_name]}
                     dict_ekranki.update(deic_el)
                 else:
-                    dict_ekranki[new_el].append(row + 2)
+                    #dict_ekranki[new_el].append(row + 2)
+                    dict_ekranki[new_el].append(jpg_name)
     except:
         pass
     #print(row + 2) # первая 0, вторая - заголовки
     row += 1
 
-#                                                     тут подсчет факт с планом
-#for el in dict_ekranki:
-    #print(el, len(dict_ekranki[el]))
+#                        тут подсчет количества скриншотов
+def count_scr_info():
+    print('Количество экранных копий по компаниям')
+    cou_ekr = 0
+    cou_otv = 0
+    for el in dict_ekranki:
+        cou_ekr += len(dict_ekranki[el])
+        print(el, len(dict_ekranki[el]))
+    print(f'\n------Всего экранных: {cou_ekr}\n')
+    print('Количество ответов по компаниям')
+    for el in dict_otveti:
+        cou_otv += len(dict_otveti[el])
+        print(el, len(dict_otveti[el]))
+    print(f'\n------Всего ответов: {cou_otv}\n')
+    print(f'\n------Итого: {cou_otv + cou_ekr}\n')
 
+#count_scr_info()
 
 import os
 import shutil
 
-main_dir = '../devfiles/ales_screenes'
-otvet_dir = '../devfiles/ales_screenes/Otveti'
-ekran_dir = '../devfiles/ales_screenes/Ekranki'
+main_dir = '../devfiles/new_ales_screenes'
+otvet_dir = main_dir + '/Otveti'
+ekran_dir = main_dir + '/Ekranki'
 
 def create_dirs():
     try:
@@ -117,6 +141,7 @@ def create_dirs():
 
 # Копируем скриншоты
 # испольюзуются: dict_ekranki, dict_otveti
+where_screens = '../devfiles/scr/new/'
 
 def copy_screen():
     copy_count = 0
@@ -125,12 +150,12 @@ def copy_screen():
         #print(direct_name)
         for num in dict_otveti[el]:
             scr_name = str(num) + '.jpg'
-            old_screen_name = '../devfiles/scr/first912/' + scr_name
-            new_screen_name = direct_name + scr_name
+            old_screen_name = where_screens + scr_name
+            new_screen_name = direct_name + 'new' + scr_name
             #print(old_screen_name)
             #print(new_screen_name)
             try:
-                shutil.copyfile(old_screen_name, new_screen_name)
+                #shutil.copyfile(old_screen_name, new_screen_name)
                 copy_count += 1
             except:
                 print("Не удалось скопировать", scr_name)
@@ -143,25 +168,32 @@ def copy_screen():
         #print(direct_name)
         for num in dict_ekranki[el]:
             scr_name = str(num) + '.jpg'
-            old_screen_name = '../devfiles/scr/first912/' + scr_name
-            new_screen_name = direct_name + scr_name
-            #print(old_screen_name)
-            #print(new_screen_name)
+            old_screen_name = where_screens + scr_name
+            new_screen_name = direct_name + 'new' +  scr_name
+            print(old_screen_name)
+            print(new_screen_name)
             try:
                 shutil.copyfile(old_screen_name, new_screen_name)
-                #print(new_screen_name)
                 copy_count_ecr += 1
             except:
                 print("Не удалось скопировать", scr_name)
     print(copy_count_ecr, ' Экранок')
 
 #create_dirs()
-#copy_screen()
+copy_screen()
 
 
 # Считаем количество использования источников {Ситилинк ООО; 100}
-dict_comp = {el : list_comp_name.count(el) for el in list_comp_name}
-sorted_dict = {}
-sorted_list = sorted(dict_comp, key = dict_comp.get, reverse = True)
-for comp in sorted_list:
-    sorted_dict[comp] = dict_comp[comp]
+def count_source_uses():
+    count = 0
+    dict_comp = {el : list_comp_name.count(el) for el in list_comp_name}
+    sorted_dict = {}
+    sorted_list = sorted(dict_comp, key = dict_comp.get, reverse = True)
+    for comp in sorted_list:
+        sorted_dict[comp] = dict_comp[comp]
+    for el in sorted_dict:
+        count += sorted_dict[el]
+        print(el, sorted_dict[el])
+    print(f'\n------Итого: {count}\n')
+
+#count_source_uses()
