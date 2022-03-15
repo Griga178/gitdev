5c5df8a619daa22cc32183585fd565ea8bbad62a
Griga178 <68722487+Griga178@users.noreply.github.com>
2022-03-15 20:06:32 +0300

    Parser class

--- parser/citi.py
+++ parser/citi.py
@@ -14,23 +14,35 @@ first_link = "https://www.citilink.ru/catalog/noutbuki/"
 link = 'https://www.dns-shop.ru/catalog/17a9de8616404e77/igry-dlya-pk/'
 
 class Just_site():
-    '''Класс для инфромации по сайтам'''
+    '''Главный класс для инфромации по сайтам'''
+    count_main_pages = 0
+
     def __init__(self, link = '', main_page = ''): #, page_type = "unknown"
         self.link = link
         self.main_page = main_page
 
-        '''
-        if type(link) = str:
-            link_list = link.split('/')
-            http_type = link_list[0] # http: or https
-            if link_list[1] == '':
-                main_page = link_list[2]
-        '''
     '''Определение куда ведет ссылка (на главную, на страницу товара
     или в каталог),
     определение главной страницы'''
     def page_info(self):
         return self.link, self.main_page
+
+class Site_info(Just_site):
+
+    count_of_links = 0
+    know_main_page = False
+    know_catalog_page = False
+    catalog_page_content = dict() # 'Ноутбуки':"https://www.citilink.ru/catalog/noutbuki/"
+    # Определяем главную страницу
+    def func_define(link):
+        ''' Определяем главную страницу '''
+        temp_list = link.split('/')
+        main_page = temp_list[0] + '//' + temp_list[2]
+        know_main_page = True
+        return main_page
+    # Определяем страницу каталога
+    # Определяем содержимое каталога
+
 
 dns = Just_site(link)
 
