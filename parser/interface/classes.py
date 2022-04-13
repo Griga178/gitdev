fd3e492fb7fa8144c1009fc9930094ce61dae0c2
Griga178 <grisha.tis@gmail.com>
2022-04-13 17:42:21 +0300

    update, separate, add categories

---
+++ parser/interface/classes.py
@@ -0,0 +1,129 @@
+class Subjects_category():
+    def __init__(self, name, other_names = False, subject_names = False, model_names = False):
+        self.name = name
+        self.other_names = other_names
+        self.subject_names = subject_names
+        self.model_names = model_names
+    def add_other_name(self):
+        pass
+    def add_subjects(self, subject_name, subjects_example):
+        if self.subject_names:
+            self.subject_names.add(subject_name)
+            subjects_example.categories.add(self.name)
+
+class Model_ver2():
+    def __init__(self, name, subject_name = False, model_parents = False, model_childs = False, model_chars = False, categories = False):
+        self.name = name# Intel Core i7 K-1255
+        self.subject_name = subject_name    # Процессор
+        self.model_parents = model_parents # {Samsung A 1225}
+        self.model_childs = model_childs
+        self.model_chars = model_chars # dict
+        self.categories = categories
+    def __str__(self):
+        return f'{self.name}'
+    def add_chars_val(self, chars_name, value, measure = '', subj_set = False):
+        if not self.model_chars:
+            self.model_chars = {chars_name: [value, measure]}
+        else:
+            self.model_chars[chars_name] = [value, measure]
+        if self.subject_name:
+            for subjects_example in subj_set:
+                if subjects_example.name == self.subject_name:
+                    subjects_example.add_chars(chars_name)
+    def description(self, space = ' - - '):
+        if self.subject_name:
+            print(f'Предмет: {self.subject_name}')
+        print(f'Название модели: {self.name}')
+        if self.categories:
+            for name in self.categories:
+                print(space, name)
+        if self.model_chars:
+            print('Характеристики:')
+            for chars_name in self.model_chars:
+                print(space, chars_name, self.model_chars[chars_name][0], self.model_chars[chars_name][1])
+
+
+class Subject_ver_3():
+    def __init__(self, name, parent = False, child = False, chars = False, models = False):
+        self.name = name
+        self.parent = parent
+        self.child = child
+        self.chars = chars
+        self.models = models
+        # self.categories = categories
+
+    def __str__(self):
+        return f'{self.name}'
+    def show_chars(self, space = ' - '):
+        if self.chars:
+            for char in self.chars:
+                print(space, char)
+    def show_parent(self):
+        if not self.parent:
+            print('Родители: -')
+        else:
+            print(f'Родители: {self.parent}')
+    def show_child(self):
+        if not self.child:
+            print('Предметов внутри: -')
+        else:
+            print(f'Предметы внутри: {self.child}')
+    def show_model(self):
+        if not self.models:
+            print('Модели внутри: -')
+        else:
+            print(f'Модели:')
+            for mod_name in self.models:
+                print(f' - {mod_name}')
+    def add_parent(self, parent_name, parent_example):
+        if not self.parent:
+            self.parent = {parent_name}
+        else:
+            self.parent.add(parent_name)
+        if not parent_example.child:
+            parent_example.child = {self.name}
+        else:
+            parent_example.child.add(self.name)
+    def add_child(self, child_name, child_example):
+        if not self.child:
+            self.child = {child_name}
+        else:
+            self.child.add(child_name)
+        if not child_example.parent:
+            child_example.parent = {self.name}
+        else:
+            child_example.parent.add(self.name)
+    def add_chars(self, chars_name):
+        if not self.chars:
+            self.chars = {chars_name}
+        else:
+            self.chars.add(chars_name)
+    def chars_description(self, examples_set, space = ' - '):
+        if space == ' - ':
+            print('Характеристики:')
+        print(f"{space}{self.name}:")
+        space += '- '
+        if self.chars:
+            for char in self.chars:
+                print(f"{space}{char}")
+        if self.child:
+            for child_name in self.child:
+                for example in examples_set:
+                    if child_name == example.name:
+                        child_example = example
+                        break
+                child_example.chars_description(examples_set, space = space)
+    def add_models(self, model_name, model_example):
+        if not self.models:
+            self.models = {model_name}
+        else:
+            self.models.add(model_name)
+        if not model_example.subject_name:
+            model_example.subject_name = self.name
+        else:
+            print(f'\nМодел принадлежит к {model_example.subject_name}')
+            # model_example.subject_name.add(self.name)
+        # Так же добавляются все ключи модели в список характеристик
+        if model_example.model_chars:
+            for chars_name in model_example.model_chars:
+                self.add_chars(chars_name)
--- parser/interface/manage_sub.py
+++ parser/interface/manage_sub.py
@@ -2,100 +2,16 @@ import sys
 sys.path.append('../')
 from data_loader import save_pkl as save
 from data_loader import load_pkl_file as load
-
-
-class Subject_ver_3():
-    def __init__(self, name, parent = False, child = False, chars = False, models = False):
-        self.name = name
-        self.parent = parent
-        self.child = child
-        self.chars = chars
-        self.models = models
-
-    def __str__(self):
-        return f'{self.name}'
-    def description(self):
-        ''' не актуально, переделать во что либо'''
-        return f'{self.name}\n Входит в состав: {[str(element) for element in self.parent] if self.parent else self.parent}\n Предметы внутри: {self.child}\n Характеристики: {self.chars}'
-    def show_parent(self):
-        print(f'Находится внутри: {self.parent}')
-        if not self.parent:
-            print('Родители: -')
-        else:
-            print(f'Родители: {self.parent}')
-    def show_child(self):
-        if not self.child:
-            print('Предметов внутри: -')
-        else:
-            print(f'Предметы внутри: {self.child}')
-    def add_parent(self, parent_name, parent_example):
-        if not self.parent:
-            self.parent = {parent_name}
-        else:
-            self.parent.add(parent_name)
-        if not parent_example.child:
-            parent_example.child = {self.name}
-        else:
-            parent_example.child.add(self.name)
-    def add_child(self, child_name, child_example):
-        if not self.child:
-            self.child = {child_name}
-        else:
-            self.child.add(child_name)
-        if not child_example.parent:
-            child_example.parent = {self.name}
-        else:
-            child_example.parent.add(self.name)
-    def add_chars(self, chars_name):
-        if not self.chars:
-            self.chars = {chars_name}
-        else:
-            self.chars.add(chars_name)
-    def chars_description(self, examples_set, space = ''):
-        print(f'{space} {self.name}')
-        space += '--'
-        if self.chars:
-            for char in self.chars:
-                print(space, char)
-        if self.child:
-            for child_name in self.child:
-                for example in examples_set:
-                    if child_name == example.name:
-                        child_example = example
-                        break
-                child_example.chars_description(examples_set, space = space)
-    def add_models(self, model_name, model_example):
-        if not self.models:
-            self.models = {model_name}
-        else:
-            self.models.add(model_name)
-        if not model_example.subject_name:
-            model_example.subject_name = {self.name}
-        else:
-            model_example.subject_name.add(self.name)
-
-
-
-class Model():
-    def __init__(self, name, subject_name = False, model_parents = False, model_childs = False, model_chars = False):
-        self.name = name# Intel Core i7 K-1255
-        self.subject_name = subject_name    # Процессор
-        self.model_parents = model_parents # {Samsung A 1225}
-        self.model_childs = model_childs
-        self.model_chars = model_chars # dict
-    def __str__(self):
-        return f'{self.name}'
+from classes import Subject_ver_3, Subjects_category, Model_ver2
 
 def recurse_interface(command = False, sec_command = False):
     pickle_file_name = 'Models_Subjects_dict'
     try:
         load_dict = load(pickle_file_name)
-        print('Загружена база!')
-
     except:
         print('Создана новая база!')
         load_dict = {'Subjects': set(), 'Models': set()}
-    print('\n - - - - - - - - - - - - - - - - - - - - ')
+    print(' - - - - - - - - - - - - - - - - - - - - ')
     if not command:
         command = input(str('Вызов подсказок: "9" \nВведите номер команды: '))
 
@@ -125,16 +41,16 @@ def recurse_interface(command = False, sec_command = False):
                 load_dict['Subjects'].add(Subject_ver_3(input_subj))
                 save(load_dict, pickle_file_name)
                 print(f'Предмет: "{input_subj}" - успешно добавлен')
-                recurse_interface('2', '1')
+                recurse_interface('2')
         elif sec_command == '2':
             input_mod = input(str('Назад - 0\nВведите название модели: '))
             if input_mod == '0':
                 recurse_interface('2')
             else:
-                load_dict['Models'].add(Model(input_mod))
+                load_dict['Models'].add(Model_ver2(input_mod))
                 save(load_dict, pickle_file_name)
                 print(f'Модель: "{input_mod}" - успешно добавлен')
-                recurse_interface('2', '2')
+                recurse_interface('2')
         elif sec_command == '0':
             recurse_interface()
         elif sec_command == '00':
@@ -146,133 +62,179 @@ def recurse_interface(command = False, sec_command = False):
     elif command == '3':
         if not sec_command:
             print('Показать описание:')
-            print('Выбрать предмет - 1\nСтарое описание - 2 или 3\n')
+            print('Выбрать предмет - 1\nВыбрать модель - 2\nСтарое описание - 3 или 4\n')
             sec_command = input(str('Введите номер команды: '))
         if sec_command == '1':
-            print("Выбери экземпляр")
-            num = 0
-            num_list = []
-            for el in load_dict['Subjects']:
-                num_list.append(el)
-                print(num, el)
-                num += 1
-            ex_num = int(input('Номер экземпляра: '))
-            chosen_example = num_list[ex_num]
+            # print("Выбери экземпляр")
+            chosen_example = chose_example(load_dict['Subjects'])
             chosen_example.chars_description(load_dict['Subjects'])
+            chosen_example.show_model()
         elif sec_command == '2':
-            for el in load_dict['Subjects']:
-                print(el.description())
+            chosen_example = chose_example(load_dict['Models'])
+            chosen_example.description()
         elif sec_command == '3':
             for el in load_dict['Subjects']:
-                print(el)
+                print(el.description())
+        elif sec_command == '4':
+            for el in load_dict['Subjects']:
+                print('\n', el)
                 el.show_child()
                 el.show_parent()
+                el.show_model()
         else:
             print("Непонятная команда")
             recurse_interface('2')
 
     elif command == '4':
-        num = 0
-        num_list = []
-        for el in load_set:
-            num_list.append(el)
-            print(num, el)
-            num += 1
-        del_num = input('что удалить? ')
-        del_num = int(del_num)
-        load_set.remove(num_list[del_num])
-        save(load_set, 'Subjects_set')
+        print("Удаление экземпляра")
+        if not sec_command:
+            sec_command = str(input('Предмета - 1\nМодели - 2\nНазад - 0\nКоманда: '))
+        if sec_command == '1':
+            sub_example = chose_example(load_dict['Subjects'])
+            if sub_example:
+                load_dict['Subjects'].remove(sub_example)
+                print(f'Предмет: {sub_example} - удален')
+                save(load_dict, pickle_file_name)
+            else:
+                print("Неудача")
+        elif sec_command == '2':
+            mod_example = chose_example(load_dict['Models'])
+            if sub_example:
+                load_dict['Models'].remove(mod_example)
+                print(f'Предмет: {mod_example} - удален')
+                save(load_dict, pickle_file_name)
+            else:
+                print("Неудача")
+        elif sec_command == '0':
+            recurse_interface()
+        else:
+            print("\nНепонятная команда")
+            recurse_interface("9")
 
     elif command == '5':
-        new_set = set()
-        for el in load_set:
-            if type(el) == 'Subject_ver_3':
-                print(el, 'update')
-                jel = Subject_ver_3(name = el.name, parent = el.parent, child = el.child, chars = el.chars, models = False)
+        if not sec_command:
+            print('Обновление классов')
+            print('Предметы - 1\nМодели - 2\nКатегории - 3\n')
+            sec_command = input(str('Введите номер команды: '))
+        if sec_command == '1':
+            new_set = set()
+            for example in load_dict['Subjects']:
+                print(example, 'update')
+                jel = Subject_ver_3(name = example.name, parent = example.parent, child = example.child, chars = example.chars, models = example.models)
                 new_set.add(jel)
-        save(new_set, 'Subjects_set')
-        print('Успешно Обновлено')
+            load_dict['Subjects'] = new_set
+            save(load_dict, pickle_file_name)
+            print('Предметы успешно Обновлены')
+        elif sec_command == '2':
+            new_set = set()
+            for example in load_dict['Models']:
+                print(example, 'update')
+                jel = Model_ver2(name = example.name, subject_name = ''.join(list(example.subject_name)) if type(example.subject_name) == set else example.subject_name, model_parents = example.model_parents,
+                    model_childs = example.model_childs, model_chars = example.model_chars, categories = False)
+            load_dict['Models'] = new_set
+            save(load_dict, pickle_file_name)
+            print('Предметы успешно Обновлены')
+        else:
+            recurse_interface()
 
     elif command == '6':
-        print("Выбери экземпляр")
-        num = 0
-        num_list = []
-        for el in load_set:
-            num_list.append(el)
-            print(num, el)
-            num += 1
-        ex_num = int(input('Номер экземпляра: '))
-        change_example = num_list[ex_num]
-        print('Что добавить?\n 1 - Родитель\n 2 - Ребенок\n 3 - Характеристика\n 4 - Модель')
-        num_com = str(input("Введите номер: "))
+        print('Работа с экземлярами')
+        if not sec_command:
+            print('Предмет - 1 - Модель -2')
+            sec_command = str(input("Введите номер: "))
+        if sec_command == '1':
+            print("Выбери экземпляр предмета")
+            change_example = chose_example(load_dict['Subjects'])
+            print('Что добавить?\n 1 - Родитель\n 2 - Ребенок\n 3 - Характеристика\n 4 - Модель')
+            third_command = str(input("Введите номер: "))
+            if third_command == '1':
+                print(f'Добавляем родителя к {change_example}\n Выбрать из списка - "1"\n Создать новый - "2"')
+                sec_num_com = str(input("Введите номер команды: "))
+                if sec_num_com == '1':
+                    parent_example = chose_example(load_dict['Subjects'])
+                    change_example.add_parent(str(parent_example), parent_example)
+                    save(load_dict, pickle_file_name)
+            elif third_command == '2':
+                print(f'Добавляем ребенка к {change_example}\n Выбрать из списка:')
+                child_example = chose_example(load_dict['Subjects'])
+                change_example.add_child(str(child_example), child_example)
+                save(load_dict, pickle_file_name)
+            elif third_command == '3':
+                print('Добавляем характеристику')
+                chars_name = str(input("Введите название характеристики: "))
+                change_example.add_chars(chars_name)
+                save(load_dict, pickle_file_name)
+            elif third_command == '4':
+                print('Добавляем модель (существующую) в экземпляр')
+                model_example = chose_example(load_dict['Models'])
+                change_example.add_models(model_example.name, model_example)
+                save(load_dict, pickle_file_name)
+        elif sec_command == '2':
+            print("Выбери экземпляр предмета")
+            model_example = chose_example(load_dict['Models'])
+            print('Что добавить?\nПредмет - 1\nХарактеристика - 2')
+            what_command = str(input('Введите номер: '))
+            if what_command == '1':
+                print('Not allowed')
+            elif what_command == '2':
+                chars_name = str(input('Название характеристики: '))
+                chars_value = str(input('Значение: '))
+                chars_measure = str(input('Единица измерения: '))
+                model_example.add_chars_val(chars_name, chars_value, chars_measure, load_dict['Subjects'])
+                save(load_dict, pickle_file_name)
 
-        if num_com == '1':
-            print(f'Добавляем родителя к {change_example}\n Выбрать из списка - "1"\n Создать новый - "2"')
-            sec_num_com = str(input("Введите номер команды: "))
-            if sec_num_com == '1':
-                sec_num = 0
-                sec_num_list = []
-                for el in load_set:
-                    sec_num_list.append(el)
-                    print(sec_num, el)
-                    sec_num += 1
-                sec_ex_num = int(input('Номер экземпляра: '))
-                parent_example = sec_num_list[sec_ex_num]
-                # print(parent_example)
-                change_example.add_parent(str(parent_example), parent_example)
-                save(load_set, 'Subjects_set')
-        elif num_com == '2':
-            print(f'Добавляем ребенка к {change_example}\n Выбрать из списка:')
-            sec_num = 0
-            sec_num_list = []
-            for el in load_set:
-                sec_num_list.append(el)
-                print(sec_num, el)
-                sec_num += 1
-            sec_ex_num = int(input('Номер экземпляра: '))
-            child_example = sec_num_list[sec_ex_num]
-            change_example.add_child(str(child_example), child_example)
-            save(load_set, 'Subjects_set')
-        elif num_com == '3':
-            print('Добавляем характеристику')
-            chars_name = str(input("Введите название характеристики: "))
-            change_example.add_chars(chars_name)
-            save(load_set, 'Subjects_set')
-        elif num_com == '4':
-            print('Добавляем модель (существующую) в экземпляр')
-            sec_num = 0
-            sec_num_list = []
-            for el in load_set:
-                sec_num_list.append(el)
-                print(sec_num, el)
-                sec_num += 1
-            sec_ex_num = int(input('Номер экземпляра: '))
-            change_example = sec_num_list[sec_ex_num]
-            sec_num = 0
-            sec_num_list = []
-            # выбор модели
-            for el in load_set:
-                sec_num_list.append(el)
-                print(sec_num, el)
-                sec_num += 1
-            model_name_num = int(input('Номер модели: '))
-            model_example = sec_num_list[model_name_num]
-            change_example.add_models(model_example.name, model_example)
-            save(load_set, 'Subjects_set')
+    elif command == '7':
+        print('Проверка характеристик')
+        for example in load_dict['Subjects']:
+            if example.chars:
+                for chars_name in example.chars:
+                    for example_two in load_dict['Subjects']:
+                        if chars_name == example_two.name:
+                            print("Совпадение! Не думаю!", chars_name)
+
+    elif command == '8':
+        print('Создать новый экземляр')
+        print('Удалить характеристику в другом экземляре')
+        print('Повторить с экземплярами моделей/предметов')
 
     elif command == '9':
         print('''\
         0 - Выход
-        1 - Показать все объекты
+        1 - Показать все экземпляры
         2 - Добавить новый экземпляр
-        3 - Показать дерево Предметов
+        3 - Показать описание экземпляров
         4 - Удалить элемент
         5 - Обновить класс
-        6 - Добавить данные в класс''')
+        6 - Добавить данные в экземпляр
+        7 - Проверка. Экземляр в характеристиках другого экземляра!
+        х1 - Изменение характеристик
+        (переместить из хар-к модели в отдельную модель)
+        х2 - Парсинг страницы характеристики
+        х3 - Парсинг каталога''')
+
     else:
         print("\nНепонятная команда")
         recurse_interface("9")
 
     recurse_interface()
 
+
+def chose_example(example_set):
+    ex_number = 0
+    temp_list = []
+    for example in example_set:
+        temp_list.append(example)
+        print(ex_number, example)
+        ex_number += 1
+    try:
+        command = int(input('Введите номер экземпляра: '))
+        # print(temp_list[command])
+        return temp_list[command]
+    except:
+        print('Не удалось выбрать экземпляр')
+        return False
+
+def buttons():
+
+    pass
 recurse_interface()
