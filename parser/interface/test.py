4b2d2a102935832dbdd4fa07b3ce729d4ba2832d
Griga178 <68722487+Griga178@users.noreply.github.com>
2022-05-18 22:45:39 +0300

    tester

--- parser/interface/test.py
+++ parser/interface/test.py
@@ -7,18 +7,20 @@ from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup
 
+import time
+
 options = webdriver.ChromeOptions()
 binary_yandex_driver_file = 'yandexdriver.exe'
 
 options.add_experimental_option('excludeSwitches', ['enable-logging']) # не выводит сообщзения в консоль
-# options.add_argument('--headless')
+options.add_argument('--headless')
 
 
 
 # не ждем полной загрузки JS
 caps = DesiredCapabilities().CHROME
-# caps["pageLoadStrategy"] = "eager"
-caps["pageLoadStrategy"] = "normal"
+caps["pageLoadStrategy"] = "eager"
+# caps["pageLoadStrategy"] = "normal"
 # Разные варианты поиска цены
 
 # Поиск по 3 тегам - есть
@@ -37,31 +39,87 @@ status_not_avaliable = 'Нет в наличии'
 status_avaliable = 'В наличии'
 status_other = 'Предзаказ'
 
-link1 = 'https://www.citilink.ru/product/ibp-powercom-spider-spd-1000n-1000va-332717/?text=Powercom+SPD-1000N'
-tag1 = ['span', 'class', 'ProductHeader__price-default_current-price']
+defender_message = ['h1', 'Проверка безопасности']
 
-link2 = 'https://www.kns.ru/product/telefon-panasonic-kx-ts2382ruw/'
-link2_not_av = 'https://www.kns.ru/product/faks-panasonic-kx-fl423ruw/'
-tag2 = ['span', 'class', 'price-org']
-tag2_n = ['h1', 'itemprop', 'name']
-tag_not_avaliable = ['div', 'class', 'goods-status'] # font-weight-bold mb-4
+manual_mode = True
 
-link3 = 'https://www.onlinetrade.ru/catalogue/smartfony-c13/zte/smartfon_zte_blade_a51_lite_2_32gb_zelenyy_zte_a51.lite.gn-2768952.html?utm_referrer=http%3a%2f%2f127.0.0.1%3a5000%2f'
-tag3 = ['div', 'class', 'catalog__displayedItem__actualPrice']
+def take_html_page(link, tag, num = False):
 
-def take_html_page(link):
     driver = webdriver.Chrome(binary_yandex_driver_file, desired_capabilities = caps, options = options)
-    # wait = WebDriverWait(driver, 500)
+    # driver = webdriver.Chrome(desired_capabilities = caps, options = options)
+    driver.set_window_size(1920, 1080)
     driver.get(link)
-    child_xpath = '//div[@class="catalog__displayedItem__actualPrice"]'
-    wait_elm = WebDriverWait(ancestor_element,10).until(EC.presence_of_element_located((By.XPATH, child_xpath)))
+
+
+    # child_xpath = f'//{tag[0]}[@{tag[1]}="{tag[2]}"]'
+    child_xpath = f'//{tag[0]}[contains(@{tag[1]}, "{tag[2]}")]'
+    try:
+        time.sleep(3)
+        try:
+            def_wait_elm = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH, '//h1[text() = "Проверка безопасности"]')))
+            print("Проверка безопасности")
+            driver.quit()
+            print('вышли из др.')
+            options.headless = False
+            print('настройки фолс.')
+            driver = webdriver.Chrome(binary_yandex_driver_file, desired_capabilities = caps, options = options)
+            print('настройки сохранили')
+            driver.get(link)
+        except:
+
+            print('не нашли форму безопасности')
+        wait_elm = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, child_xpath)))
+        html_page = driver.page_source
+        soup = BeautifulSoup(html_page, 'html.parser')
+        content = soup.find(tag[0], attrs={tag[1]: tag[2]})
+        # content = wait_elm.get_attribute('innerHTML')
+    except:
+        print(driver.page_source)
+        content = 'ДОЛГО ЖДАТЬ!'
+
+    # print([wait_elm.text])
+    print([content])
     # wait.until(EC.element_to_be_clickable((By.ID, "text")))
-    html_page = driver.page_source
+    #
 
     driver.quit()
-    return html_page
+    # return html_page
 
-# Запуск:
-html_string_page = take_html_page(link3)
 
-three_tag_search(html_string_page, tag3)
+
+
+
+def tester():
+    num = 0
+    start_time = time.time()
+    link1 = 'https://www.citilink.ru/product/ibp-powercom-spider-spd-1000n-1000va-332717/?text=Powercom+SPD-1000N'
+    tag1 = ['span', 'class', 'ProductHeader__price-default_current-price']
+
+    link2 = 'https://www.kns.ru/product/telefon-panasonic-kx-ts2382ruw/'
+    link2_not_av = 'https://www.kns.ru/product/faks-panasonic-kx-fl423ruw/'
+    tag2 = ['span', 'class', 'price-org']
+    tag2_n = ['h1', 'itemprop', 'name']
+    tag_not_avaliable = ['div', 'class', 'goods-status'] # font-weight-bold mb-4
+
+    link3 = 'https://www.onlinetrade.ru/catalogue/smartfony-c13/zte/smartfon_zte_blade_a51_lite_2_32gb_zelenyy_zte_a51.lite.gn-2768952.html'
+    tag3 = ['div', 'class', 'catalog__displayedItem__actualPrice']
+
+    # УСПЕХ:
+    print('# 1')
+    take_html_page(link1, tag1, 1)
+    # УСПЕХ:
+    print('# 2')
+    take_html_page(link2, tag2, 2)
+    print('# 3')
+    take_html_page(link2, tag2_n, 3)
+    print('# 4')
+    take_html_page(link2_not_av, tag_not_avaliable, 4)
+    # УСПЕХ:
+    # Надо ждать загрузки JS дольше обычного, используя "wait"
+    print('# 5')
+    take_html_page(link3, tag3, 5)
+
+    cur_sec = round((time.time() - start_time), 2)
+    print(f'Вревмя выполнения: {int(cur_sec // 60)} мин. {cur_sec} сек.)')
+
+tester()
