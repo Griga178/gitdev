c03cfe12b9d95379ce5685138c3e8177d325448e
Griga178 <grisha.tis@gmail.com>
2024-03-05 09:54:25 +0300

    test - completed

--- ya/test4.py
+++ ya/test4.py
@@ -1,3 +1,4 @@
+# Решение задачи 
 variables = ['Window','Bird','Food','Human']
 # variables = [input() for i in range(4)]
 # with open('input.txt') as f:
@@ -29,10 +30,16 @@ class MyDict(dict):
 url = 'http://127.0.0.1:7777/'
 s = requests.Session()
 
-def do_mew(i = 0, j = -1):
+def do_mew(i = 0, j = -1, elements = False):
     req = requests.Request('MEW', url)
     prepped = req.prepare()
-    prepped.headers = MyDict('x-cat-variable', variables[i:j])
+    send_variables = []
+    if elements:
+        for index in elements:
+            send_variables.append(variables[index])
+    else:
+        send_variables = variables[i:j]
+    prepped.headers = MyDict('x-cat-variable', send_variables)
     resp = s.send(prepped)
     resp = {k.lower(): v for k, v in resp.headers.items()}
 
@@ -40,79 +47,132 @@ def do_mew(i = 0, j = -1):
     values = [i.strip() for i in values]
     return values
 
-# variables = [sys.stdin.readline().strip() for i in range(4)]
+variables = [sys.stdin.readline().strip() for i in range(4)]
 final_values = []
 
 
-# 1-й Запрос к 1-3 позициям
+# Запросы + обработка (1: ABC, 2: BCD, 3: ABD)
 query_1 = do_mew(0, 3)
 if len(set(query_1)) == 1:
     # 1-3 значения одинаковы
-    final_values = query_1
+    val_1, val_2, val_3 = query_1
     query_2 = do_mew(3, 4)
-    final_values.append(query_2[0])
+    val_4 = query_2[0]
 
 elif len(set(query_1)) == 2:
     # 1-3 значения делятся на 2 типа
-    # находим значение 1 элемента
-    query_2 = do_mew(0, 1)
-    val_1 = query_2[0]
+    query_2 = do_mew(1, 4)
+    if len(set(query_2)) == 1:
+        val_2 = val_3 =  val_4 = query_2[0]
+        val_1 = list(set(query_1) - {val_2})[0]
 
-    if query_1.count(val_1) == 1:
-        # 2 и 3 значения - одинаковы
-        val_2_and_3 = list(set(query_1) - set(val_1))[0]
-        # находим 4 значение
-        query_3 = do_mew(3, 4)
-        val_4 = query_3[0]
-        final_values = [val_1, val_2_and_3, val_2_and_3, val_4]
-    else:
-        # 2 и 3 значения - разные --> смотрим 3-й и 4-й
-        # одно из них = значению 1-ого элемента
-        query_3 = do_mew(2, 4)
-        if len(set(query_3)) == 1:
-            # 3 и 4 значения - одинаковы
-            val_3_and_4 = list(set(query_3))[0]
-            ''' !!!!!!!!!! - ? '''
-            val_2 = list(set(query_1) - set(val_1))[0]
-            final_values = [val_1, val_2, val_3_and_4, val_3_and_4]
+    elif len(set(query_2)) == 2:
+        if set(query_2) != set(query_1):
+
+            val_4 = list(set(query_2) - set(query_1))[0]
+            val_3 = val_2 = list(set(query_2) - {val_4})[0]
+            query_1.remove(val_3)
+            query_1.remove(val_2)
+            val_1 = query_1[0]
         else:
-            # 3 и 4 - разные
-            ''' !!!!!!!!!! '''
-            val_3 = list(set(query_3) & set(query_1))[0]
-            val_4 = list(set(query_3) - set(val_3))[0]
-            val_2 = list(set(query_1) - set(val_3))[0]
-            final_values = [val_1, val_2, val_3, val_4]
+            query_3 = do_mew(elements = [0, 1, 3])
 
+            if  len(set(query_3)) == 1:
+                val_1 = val_2 = val_4 = list(set(query_3))[0]
+                val_3 = list(set(query_1) - {val_2})[0]
+            elif query_1 != query_2:
+                for el in query_1:
+                    if query_1.count(el) == 2:
+                        double_in_1 = el
+                        break
+                for el in query_2:
+                    if query_2.count(el) == 2:
+                        double_in_2 = el
+                        break
+                val_1 = double_in_1
+                val_4 = double_in_2
+                query_3.remove(val_1)
+                query_3.remove(val_4)
+                val_2 = query_3[0]
+                query_1.remove(val_1)
+                query_1.remove(val_2)
+                val_3 = query_1[0]
+
+            elif query_2 != query_3:
+                for el in query_1:
+                    if query_1.count(el) == 2:
+                        double_in_1 = el
+                        break
+                for el in query_3:
+                    if query_3.count(el) == 2:
+                        double_in_3 = el
+                        break
+                val_3 = double_in_1
+                val_4 = double_in_3
+                query_2.remove(val_3)
+                query_2.remove(val_4)
+                val_2 = query_2[0]
+                query_1.remove(val_2)
+                query_1.remove(val_3)
+                val_1 = query_1[0]
+            else:
+                for el in query_1:
+                    if query_1.count(el) == 1:
+                        val_unic = el
+                        break
+                val_2 = val_unic
+                val_1 = val_3 = val_4 = list(set(query_1) - {val_2})[0]
+
+    elif len(set(query_2)) == 3:
+        val_4 = list(set(query_2) - set(query_1))[0]
+        for el in query_1:
+            if query_1.count(el) == 2:
+                double_in_1 = el
+                break
+        val_1 = double_in_1
+        query_3 = do_mew(1, 2)
+        val_2 = query_3[0]
+        val_3 = list(set(query_2) - {val_4} - {val_2})[0]
 
 elif len(set(query_1)) == 3:
     # 1,2,3 значения разные --> смотрим на 2,3,4
     query_2 = do_mew(1, 4)
     if len(set(query_2)) == 2:
-        # знаем значение 1-ого элемента
+        # Всего 3 типа значений
+        for el in query_2:
+            if query_2.count(el) == 2:
+                double_in_2 = el
+                break
+        val_4 = double_in_2
         val_1 = list(set(query_1) - set(query_2))[0]
-        # смотрим значение 2 элемента
-        query_3 = do_mew(1, 4)
-        val_2 = query_3[0]
-        if query_3.count(val_2) == 1:
-            # 3 и 4 значения - одинаковы
-            val_3_and_4 = list(set(query_2) - set(val_2))[0]
-            final_values = [val_1, val_2, val_3_and_4, val_3_and_4]
-        else:
-            # 2 и 4 значения - одинаковы
-            val_4 = query_3[0]
-            val_3 = list(set(query_2) - set(val_2))[0]
-            final_values = [val_1, val_2, val_3, val_4]
-
-    else:
-        # знаем 1 и 4 значения
-        val_1 = list(set(query_1) - set(query_2))[0]
-        val_4 = list(set(query_2) - set(query_1))[0]
-        # находим 2 значение
         query_3 = do_mew(1, 2)
         val_2 = query_3[0]
-        val_3 = list((set(query_2) & set(query_1) - set(query_3)))[0]
-        final_values = [val_1, val_2, val_3, val_4]
+        query_1.remove(val_1)
+        query_1.remove(val_2)
+        val_3 = query_1[0]
+    else:
+        if query_1 == query_2:
+            query_3 = do_mew(elements = [0, 1, 3])
+            # По логике:  query_1 != query_3
+            # ==> query_2 != query_3
+            for el in query_3:
+                if query_3.count(el) == 2:
+                    double_in_3 = el
+                    break
+            val_4 = double_in_3
+            val_3 = list(set(query_1) - set(query_3))[0]
+            # в этом случае 1-й == 4-ому
+            val_1 = val_4
+            val_2 = list(set(query_1) - {val_1} - {val_3})[0]
+        else:
+            # Всего 4 типа значений
+            val_1 = list(set(query_1) - set(query_2))[0]
+            val_4 = list(set(query_2) - set(query_1))[0]
+            query_3 = do_mew(1, 2)
+            val_2 = query_3[0]
+            val_3 = list(set(query_1) - {val_2} - {val_1})[0]
 
+final_values = [val_1, val_2, val_3, val_4]
 
 for sval in final_values:
     val = ''.join(sval)
--- ya/tt4.py
+++ ya/tt4.py
@@ -1,3 +1,4 @@
+# Тест 4 задачи
 my_keys = ['A', 'B', 'C', 'D']
 
 combination = []
@@ -15,13 +16,18 @@ for a in range(1, 5):
                 combination.append(srv_dict)
 
 
-def emu_mew(combo, i = 0, j = -1):
-    query = my_keys[i:j]
+def emu_mew(combo, i = 0, j = -1, elements = False):
     variables = []
-    for el in query:
-        variables.append(combo[el])
-    values = sorted(variables)
+    if elements:
+        for ie in elements:
+            key = my_keys[ie]
+            variables.append(combo[key])
+    else:
+        query = my_keys[i:j]
+        for el in query:
+            variables.append(combo[el])
 
+    values = sorted(variables)
     return values
 
 def check(combo, answer):
@@ -34,14 +40,13 @@ def check(combo, answer):
         i += 1
     print('Error:', combo, answer) if wrong else None
 
-
 def main(combo):
     query_1 = emu_mew(combo, 0, 3)
     if len(set(query_1)) == 1:
         # 1-3 значения одинаковы
-        final_values = query_1
+        val_1, val_2, val_3 = query_1
         query_2 = emu_mew(combo, 3, 4)
-        final_values.append(query_2[0])
+        val_4 = query_2[0]
 
     elif len(set(query_1)) == 2:
         # 1-3 значения делятся на 2 типа
@@ -58,99 +63,105 @@ def main(combo):
                 query_1.remove(val_3)
                 query_1.remove(val_2)
                 val_1 = query_1[0]
-            # elif set(query_2) != set(query_1):
             else:
-                if query_1 == query_2:
-                    # print('ТУТ', combo)
-                    # for el in query_1:
-                    #     if query_1.count(el) == 2:
-                    #         double_1 = el
-                    # for el in query_2:
-                    #     if query_2.count(el) == 2:
-                    #         double_2 = el
-                    # val_1 = double_1
-                    # val_4 = double_2
-                    #
-                    # query_3 = emu_mew(combo, 1, 2)
-                    # val_2 = query_3[0]
-                    # query_1.remove(val_1)
-                    # query_1.remove(val_2)
-                    # val_3 = query_1[0]
-                     query_3 = emu_mew(combo, 0, 2)
-                     if len(set(query_3)) == 1:
-                         val_1 = val_2 = list(set(query_3))[0]
-                         val_3 = list(set(query_1) - {val_1})[0]
-                         query_2.remove(val_3)
-                         query_2.remove(val_2)
-                         val_4 = query_2[0]
-                     else:
-                        for el in query_1:
-                            if query_1.count(el) == 2:
-                                double_1 = el
+                query_3 = emu_mew(combo, elements = [0, 1, 3])
 
-                        val_3 = double_1
-                        val_1 = 5
-                        val_2 = 5
-                        val_4 = 5
+                if  len(set(query_3)) == 1:
+                    val_1 = val_2 = val_4 = list(set(query_3))[0]
+                    val_3 = list(set(query_1) - {val_2})[0]
+                elif query_1 != query_2:
+                    for el in query_1:
+                        if query_1.count(el) == 2:
+                            double_in_1 = el
+                            break
+                    for el in query_2:
+                        if query_2.count(el) == 2:
+                            double_in_2 = el
+                            break
+                    val_1 = double_in_1
+                    val_4 = double_in_2
+                    query_3.remove(val_1)
+                    query_3.remove(val_4)
+                    val_2 = query_3[0]
+                    query_1.remove(val_1)
+                    query_1.remove(val_2)
+                    val_3 = query_1[0]
+
+                elif query_2 != query_3:
+                    for el in query_1:
+                        if query_1.count(el) == 2:
+                            double_in_1 = el
+                            break
+                    for el in query_3:
+                        if query_3.count(el) == 2:
+                            double_in_3 = el
+                            break
+                    val_3 = double_in_1
+                    val_4 = double_in_3
+                    query_2.remove(val_3)
+                    query_2.remove(val_4)
+                    val_2 = query_2[0]
+                    query_1.remove(val_2)
+                    query_1.remove(val_3)
+                    val_1 = query_1[0]
                 else:
-                    '''     !!!!         '''
-                    return
+                    for el in query_1:
+                        if query_1.count(el) == 1:
+                            val_unic = el
+                            break
+                    val_2 = val_unic
+                    val_1 = val_3 = val_4 = list(set(query_1) - {val_2})[0]
 
         elif len(set(query_2)) == 3:
-            if len(set(query_1) | set(query_2)) == 2:
-                for el in query_1:
-                    if query_1.count(el) == 2:
-                        val_1 = el
-                val_4 = list(set(query_2) - set(query_1))[0]
-                query_3 = emu_mew(combo, 1, 2)
-                val_2 = query_3[0]
-                query_2.remove(val_4)
-                query_2.remove(val_2)
-                val_3 = query_2[0]
+            val_4 = list(set(query_2) - set(query_1))[0]
+            for el in query_1:
+                if query_1.count(el) == 2:
+                    double_in_1 = el
+                    break
+            val_1 = double_in_1
+            query_3 = emu_mew(combo, 1, 2)
+            val_2 = query_3[0]
+            val_3 = list(set(query_2) - {val_4} - {val_2})[0]
 
-            elif len(set(query_1) | set(query_2)) == 3:
-                '''     !!!!         '''
-                return
-            else:
-                '''     !!!!         '''
-                return
-
-        final_values = [val_1, val_2, val_3, val_4]
-    else:
-        return
-
-    if True:
-        pass
     elif len(set(query_1)) == 3:
         # 1,2,3 значения разные --> смотрим на 2,3,4
         query_2 = emu_mew(combo, 1, 4)
         if len(set(query_2)) == 2:
-            # знаем значение 1-ого элемента
+            # Всего 3 типа значений
+            for el in query_2:
+                if query_2.count(el) == 2:
+                    double_in_2 = el
+                    break
+            val_4 = double_in_2
             val_1 = list(set(query_1) - set(query_2))[0]
-            # смотрим значение 2 элемента
-            query_3 = emu_mew(combo, 1, 4)
-            val_2 = query_3[0]
-            if query_3.count(val_2) == 1:
-                # 3 и 4 значения - одинаковы
-                val_3_and_4 = list(set(query_2) - set(val_2))[0]
-                final_values = [val_1, val_2, val_3_and_4, val_3_and_4]
-            else:
-                # 2 и 4 значения - одинаковы
-                val_4 = query_3[0]
-                val_3 = list(set(query_2) - set(val_2))[0]
-                final_values = [val_1, val_2, val_3, val_4]
-
-        else:
-            # знаем 1 и 4 значения
-            val_1 = list(set(query_1) - set(query_2))[0]
-            val_4 = list(set(query_2) - set(query_1))[0]
-            # находим 2 значение
             query_3 = emu_mew(combo, 1, 2)
             val_2 = query_3[0]
-            val_3 = list((set(query_2) & set(query_1) - set(query_3)))[0]
-            final_values = [val_1, val_2, val_3, val_4]
+            query_1.remove(val_1)
+            query_1.remove(val_2)
+            val_3 = query_1[0]
+        else:
+            if query_1 == query_2:
+                query_3 = emu_mew(combo, elements = [0, 1, 3])
+                # По логике:  query_1 != query_3
+                # ==> query_2 != query_3
+                for el in query_3:
+                    if query_3.count(el) == 2:
+                        double_in_3 = el
+                        break
+                val_4 = double_in_3
+                val_3 = list(set(query_1) - set(query_3))[0]
+                # в этом случае 1-й == 4-ому
+                val_1 = val_4
+                val_2 = list(set(query_1) - {val_1} - {val_3})[0]
+            else:
+                # Всего 4 типа значений
+                val_1 = list(set(query_1) - set(query_2))[0]
+                val_4 = list(set(query_2) - set(query_1))[0]
+                query_3 = emu_mew(combo, 1, 2)
+                val_2 = query_3[0]
+                val_3 = list(set(query_1) - {val_2} - {val_1})[0]
 
-
+    final_values = [val_1, val_2, val_3, val_4]
     check(combo, final_values)
 
 counter = 0
